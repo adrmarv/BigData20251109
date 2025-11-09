2d7c63bbb10b8553c0737bc141c18124da7b2a53
AZ <adrian.marvolo.zieba@gmail.com>
2025-11-09 12:24:54 +0100

    hej

--- application.py
+++ application.py
@@ -5,12 +5,10 @@ import jsonify
 
 application = Flask(__name__)
 
-ENV. os.environ.get('Flask_ENV', 'dev') #pobierz mi zmienna srodowiskowa ale jezeli jej nie znajdziesz to pryjmij ze to jest dev
+ENV.os.environ.get('Flask_ENV', 'dev') #pobierz mi zmienna srodowiskowa ale jezeli jej nie znajdziesz to pryjmij ze to jest dev
 
 
-@application.route('/json')
-def get_json():
-    return jsonify({"FLASK_ENV":ENV})
+
 
 
 @application.route('/')
@@ -38,6 +36,13 @@ def hello_world():
             </style>"""
 
     return text
+
+@application.route('/json')
+def get_json():
+    return jsonify({"FLASK_ENV": ENV})
+
+
+
 #nie dziala mi to an domyslnym 127.0.0.1:5000 bo projekt nie mam na puplcie tylko w jakims kataalogu, czytalem ze jest an to obesjcie
 if __name__ == "main__":
     application.run(debug=True) #na produkcji powinno to byc wylaczone, wiec dobrze miec taki sam ale drugi plik na proda z tym aylczanym
