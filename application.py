da17fbb61ce6cb1a4c2632e504db0f81b03439e3
AZ <adrian.marvolo.zieba@gmail.com>
2025-11-09 10:50:04 +0100

    hej

---
+++ application.py
@@ -0,0 +1,30 @@
+#n9ie bedzie dzial na tym
+
+from flask import Flask
+
+application = Flask(__name__)
+
+@application.route('/')
+def hello_world():
+    text = """<!DOCTYPE html>
+    
+    <html>
+        <head>
+            <title> Witaj swiecie big data</title>
+        </head>
+        <body>
+            
+            <h1>Witaj swiecie! ibig data z pythonem</h1>
+           <p>Procesy CI/CD - autmmatyczne wdrażanie</p>
+        </body>
+    
+    </html>
+
+    
+    """
+    return text
+
+if __name__ == "main__":
+    application.run(debug=True) #na produkcji powinno to byc wylaczone, wiec dobrze miec taki sam ale drugi plik na proda z tym aylczanym
+
+
